3e78b401734146bc3444f7f1a3463621f6ccf8c3
Antonio G <tonyg@Mac-2.local>
2021-08-20 10:55:18 -0500

    Initial files

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import Profile, Post, Relationship
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Relationship)
--- twitter/views.py
+++ twitter/views.py
@@ -1,106 +1,18 @@
-from django.shortcuts import render, redirect
-from .models import Profile, Post, Relationship
-from .forms import UserRegisterForm, PostForm, ProfileUpdateForm, UserUpdateForm
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
-@login_required
+
 def home(request):
-	posts = Post.objects.all()
-	if request.method == 'POST':
-		form = PostForm(request.POST)
-		if form.is_valid():
-			post = form.save(commit=False)
-			post.user = request.user
-			post.save()
-			return redirect('home')
-	else:
-		form = PostForm()
+    return render(request, 'twitter/newsfeed.html')
 
-	context = {'posts':posts, 'form' : form }
-	return render(request, 'twitter/newsfeed.html', context)
 
 def register(request):
-	if request.method == 'POST':
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('home')
-	else:
-		form = UserRegisterForm()
-
-	context = {'form' : form}
-	return render(request, 'twitter/register.html', context)
+    return render(request, 'twitter/register.html')
 
 
-def delete(request, post_id):
-	post = Post.objects.get(id=post_id)
-	post.delete()
-	return redirect('home')
+def profile(request):
+
+    return render(request, 'twitter/profile.html')
 
 
-def profile(request, username):
-	user = User.objects.get(username=username)
-	posts = user.posts.all()
-	context = {'user':user, 'posts':posts}
-	return render(request, 'twitter/profile.html', context)
-
-@login_required
 def editar(request):
-	if request.method == 'POST':
-		u_form = UserUpdateForm(request.POST, instance=request.user)
-		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
-		if u_form.is_valid() and p_form.is_valid():
-			u_form.save()
-			p_form.save()
-			return redirect('home')
-	else:
-		u_form = UserUpdateForm(instance=request.user)
-		p_form = ProfileUpdateForm()
-
-	context = {'u_form' : u_form, 'p_form' : p_form}
-	return render(request, 'twitter/editar.html', context)
-
-@login_required
-def follow(request, username):
-	current_user = request.user
-	to_user = User.objects.get(username=username)
-	to_user_id = to_user
-	rel = Relationship(from_user=current_user, to_user=to_user_id)
-	rel.save()
-	return redirect('home')
-
-@login_required
-def unfollow(request, username):
-	current_user = request.user
-	to_user = User.objects.get(username=username)
-	to_user_id = to_user.id
-	rel = Relationship.objects.get(from_user=current_user.id, to_user=to_user_id)
-	rel.delete()
-	return redirect('home')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return render(request, 'twitter/editar.html')
